8b9d41a8812965f92f3cd1e6054c88f8fe65edbb
soubhagya <soubhagya.r.nayak@gmail.com>
2019-09-16 00:25:52 +0530

    Added solution for problem 113: Rotate words of string
    Added solution for problem 113: Rotate words of string

---
+++ DailyCodingProblem/Problem113.cs
@@ -0,0 +1,46 @@
+﻿namespace DailyCodingProblem
+{
+    /*
+     * This problem was asked by Google.
+     * Given a string of words delimited by spaces, 
+     *  reverse the words in string. 
+     *  For example, given "hello world here", return "here world hello"
+     * Follow-up: given a mutable string representation, 
+     *  can you perform this operation in-place?
+     * 
+     * MEDIUM
+     */
+    public static class Problem113
+    {
+        public static string ReverseWords(string input)
+        {
+            if(input == null || input.Length <= 1)
+            {
+                return input;
+            }
+            char[] chars = input.ToCharArray();
+            int start = 0;
+            for(int i = 0;i < chars.Length; i++)
+            {
+                if(chars[i] == ' ')
+                {
+                    Reverse(chars, start, i - 1);
+                    start = i + 1;
+                }
+            }
+            Reverse(chars, start, chars.Length - 1);
+            Reverse(chars, 0, chars.Length - 1);
+            return new string(chars);
+        }
+
+        private static void Reverse(char[] input, int start, int end)
+        {
+            for (int i = start, j = end;i < j; i++, j--)
+            {
+                char temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+    }
+}
---
+++ DailyCodingProblemTests/Problem113Test.cs
@@ -0,0 +1,17 @@
+﻿using DailyCodingProblem;
+using NUnit.Framework;
+
+namespace DailyCodingProblemTests
+{
+    public class Problem113Test
+    {
+        [Test]
+        public void TestReverseWords()
+        {
+            string output = Problem113.ReverseWords("hello world here");
+            Assert.AreEqual("here world hello", output);
+            output = Problem113.ReverseWords("soubhagya r nayak ");
+            Assert.AreEqual(" nayak r soubhagya", output);
+        }
+    }
+}
