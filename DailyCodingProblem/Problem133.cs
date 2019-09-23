85e9b4d9c3f613e23d0e2e347ea6e45361971884
soubhagya <soubhagya.r.nayak@gmail.com>
2019-09-23 08:05:21 +0530

    Added solution to Problem 133: Inorder successor of BST
    Added solution to Problem 133: Inorder successor of BST.

---
+++ DailyCodingProblem/Problem133.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * This problem was asked by Amazon.
+ * Given a node in a binary search tree, return the next bigger element, also known as the inorder successor.
+ * For example, the inorder successor of 22 is 30.
+   10
+  /  \
+ 5    30
+     /  \
+   22    35
+   You can assume each node has a parent pointer.
+ * 
+ */
+namespace DailyCodingProblem
+{
+    public class BstNode
+    {
+        public BstNode Left { get; set; }
+
+        public BstNode Right { get; set; }
+
+        public int Value { get; set; }
+
+        public BstNode(int value)
+        {
+            this.Value = value;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+
+    public static class Problem133
+    {
+        public static BstNode FindInOrderSuccessor(BstNode node, BstNode root)
+        {
+            BstNode result = null;
+            if(node.Right == null)
+            {
+                BstNode current = root;
+                while (true)
+                {
+                    if(current.Value == node.Value)
+                    {
+                        break;
+                    }
+                    if (current.Value > node.Value)
+                    {
+                        result = current;
+                        current = current.Left; 
+                    }
+                    else if(current.Value < node.Value)
+                    {
+                        current = current.Right;
+                    }
+                }
+            }
+            else
+            {
+                result = node.Right;
+                while(result.Left != null)
+                {
+                    result = result.Left;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ DailyCodingProblemTests/Problem133Test.cs
@@ -0,0 +1,28 @@
+﻿using DailyCodingProblem;
+using NUnit.Framework;
+
+namespace DailyCodingProblemTests
+{
+    public class Problem133Test
+    {
+        [Test]
+        public void TestFindInOrderSuccessor()
+        {
+            BstNode root = new BstNode(10);
+            root.Left = new BstNode(5);
+            root.Right = new BstNode(30);
+            root.Right.Left = new BstNode(22);
+            root.Right.Right = new BstNode(35);
+            BstNode result = Problem133.FindInOrderSuccessor(root.Left, root);
+            Assert.AreEqual(10, result.Value);
+            result = Problem133.FindInOrderSuccessor(root.Right.Left, root);
+            Assert.AreEqual(30, result.Value);
+            result = Problem133.FindInOrderSuccessor(root.Right, root);
+            Assert.AreEqual(35, result.Value);
+            result = Problem133.FindInOrderSuccessor(root.Right.Right, root);
+            Assert.AreEqual(null, result);
+            result = Problem133.FindInOrderSuccessor(root, root);
+            Assert.AreEqual(22, result.Value);
+        }
+    }
+}
