84ee0c5d14816670de8c2319910d0e3f38bf19eb
soubhagya <soubhagya.r.nayak@gmail.com>
2019-09-29 18:08:56 +0530

    Added solution to Problem 110: Find all paths from root to leaf in binary tree
    Added solution to Problem 110: Find all paths from root to leaf in binary tree

---
+++ DailyCodingProblem/Problem110.cs
@@ -0,0 +1,69 @@
+﻿
+/*
+ * This problem was asked by Facebook.
+ * Given a binary tree, return all paths from the root to leaves.
+ * For example, given the tree:
+
+   1
+  / \
+ 2   3
+    / \
+   4   5
+   Return [[1, 2], [1, 3, 4], [1, 3, 5]].
+ * 
+ */
+using System.Collections.Generic;
+
+namespace DailyCodingProblem
+{
+    public static class Problem110
+    {
+        public static List<List<int>> FindAllPathsFromRootToLeafs(BinaryTreeNode root)
+        {
+            List<List<int>> paths = new List<List<int>>();
+            if(root == null)
+            {
+                return paths;
+            }
+            List<int> pathFromRoot = new List<int>();
+            return FindAllPathsFromRootToLeafs(root, pathFromRoot);
+        }
+
+        private static List<List<int>> FindAllPathsFromRootToLeafs(BinaryTreeNode node, List<int> pathFromRoot)
+        {
+            List<List<int>> paths = new List<List<int>>();
+            var temp = new List<int>(pathFromRoot);
+            temp.Add(node.Value);
+            if (node.Left == null && node.Right == null)
+            {
+                paths.Add(temp);
+            }
+            else
+            {
+                if(node.Left != null)
+                {
+                    paths.AddRange(FindAllPathsFromRootToLeafs(node.Left, temp));
+                }
+                if(node.Right != null)
+                {
+                    paths.AddRange(FindAllPathsFromRootToLeafs(node.Right, temp));
+                }
+            }
+            return paths;
+        }
+    }
+
+    public class BinaryTreeNode
+    {
+        public BinaryTreeNode Left { get; set; }
+
+        public BinaryTreeNode Right { get; set; }
+
+        public int Value { get; set; }
+
+        public BinaryTreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ DailyCodingProblemTests/Problem110Test.cs
@@ -0,0 +1,29 @@
+﻿using DailyCodingProblem;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DailyCodingProblemTests
+{
+    public class Problem110Test
+    {
+        [Test]
+        public void TestFindAllPathsFromRootToLeafs()
+        {
+            List<List<int>> paths = Problem110.FindAllPathsFromRootToLeafs(null);
+            Assert.AreEqual(0, paths.Count);
+
+            paths = Problem110.FindAllPathsFromRootToLeafs(new BinaryTreeNode(10));
+            Assert.AreEqual(1, paths.Count);
+            Assert.AreEqual(new List<int>() { 10 }, paths[0]);
+
+            BinaryTreeNode root = new BinaryTreeNode(1);
+            root.Left = new BinaryTreeNode(2);
+            root.Right = new BinaryTreeNode(3);
+            root.Right.Left = new BinaryTreeNode(4);
+            root.Right.Right = new BinaryTreeNode(5);
+
+            paths = Problem110.FindAllPathsFromRootToLeafs(root);
+            Assert.AreEqual(3, paths.Count);
+        }
+    }
+}
