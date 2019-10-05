3eaecc0d9aeb095258c0f76acd2d4c9b55f0c01e
soubhagya <soubhagya.r.nayak@gmail.com>
2019-10-05 23:20:00 +0530

    Added solution for Problem 146: Prune a binary tree
    Added solution for Problem 146: Prune a binary tree

---
+++ DailyCodingProblem/Problem146.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/*
+ * This question was asked by BufferBox.
+ * Given a binary tree where all nodes are either 0 or 1, prune the tree so that subtrees containing all 0s are removed.
+ * For example, given the following tree:
+
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 0   0
+ should be pruned to:
+
+   0
+  / \
+ 1   0
+    /
+   1
+   
+    We do not remove the tree at the root or its left child because it still has a 1 as a descendant.
+ * 
+ * 
+ * MEDIUM
+ * 
+ */
+namespace DailyCodingProblem
+{
+    public static class Problem146
+    {
+        public static BinaryTreeNode Prune(BinaryTreeNode root)
+        {
+            PruneInternal(root);
+            return root;
+        }
+
+        private static bool PruneInternal(BinaryTreeNode node)
+        {
+            if(node == null)
+            {
+                return true;
+            }
+            if (PruneInternal(node.Left))
+            {
+                node.Left = null;
+            }
+            if (PruneInternal(node.Right))
+            {
+                node.Right = null;
+            }
+            return (node.Value==0) && (node.Left==null) && (node.Right==null);
+        }
+    }
+}
---
+++ DailyCodingProblemTests/Problem146Test.cs
@@ -0,0 +1,28 @@
+﻿using DailyCodingProblem;
+using NUnit.Framework;
+
+namespace DailyCodingProblemTests
+{
+    public class Problem146Test
+    {
+        [Test]
+        public void TestPrune()
+        {
+            BinaryTreeNode root = new BinaryTreeNode(0);
+            root.Left = new BinaryTreeNode(1);
+            root.Right = new BinaryTreeNode(0);
+            root.Right.Left = new BinaryTreeNode(1);
+            root.Right.Right = new BinaryTreeNode(0);
+            root.Right.Left.Left = new BinaryTreeNode(0);
+            root.Right.Left.Right = new BinaryTreeNode(0);
+            var result = Problem146.Prune(root);
+            Assert.AreEqual(0, root.Value);
+            Assert.AreEqual(1, root.Left.Value);
+            Assert.AreEqual(0, root.Right.Value);
+            Assert.AreEqual(1, root.Right.Left.Value);
+            Assert.IsNull(root.Right.Right);
+            Assert.IsNull(root.Right.Left.Left);
+            Assert.IsNull(root.Right.Left.Right);
+        }
+    }
+}
