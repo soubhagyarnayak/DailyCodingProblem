c64cfdf96db8ed5995b56d9225955465f1e363d4
soubhagya <soubhagya.r.nayak@gmail.com>
2019-10-23 19:47:09 +0530

    Added a solution to problem 163: Reverse Polish Notation
    Added a solution to problem 163: Reverse Polish Notation

---
+++ DailyCodingProblem/Problem163.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * This problem was asked by Jane Street.
+ * Given an arithmetic expression in Reverse Polish Notation, write a program to evaluate it.
+ * The expression is given as a list of numbers and operands. For example: [5, 3, '+'] should return 5 + 3 = 8.
+ * or example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
+ * You can assume the given expression is always valid.
+ * 
+ * HARD
+ * 
+ */
+
+namespace DailyCodingProblem
+{
+    public static class Problem163
+    {
+        public static double EvaluateReversePolishNotation(string[] input)
+        {
+            if(input == null || input.Length == 0)
+            {
+                throw new ArgumentException("The specified input to evaluate is null or empty");
+            }
+            
+            Stack<double> operands = new Stack<double>();
+            for (int i = 0; i <input.Length; i++)
+            {
+                string data = input[i];
+                switch (data)
+                {
+                    case "+":
+                        double right = operands.Pop();
+                        double left = operands.Pop();
+                        operands.Push(left + right);
+                        break;
+                    case "-":
+                        right = operands.Pop();
+                        left = operands.Pop();
+                        operands.Push(left - right);
+                        break;
+                    case "*":
+                        right = operands.Pop();
+                        left = operands.Pop();
+                        operands.Push(left * right);
+                        break;
+                    case "/":
+                        right = operands.Pop();
+                        left = operands.Pop();
+                        operands.Push(left / right);
+                        break;
+                    default:
+                        operands.Push(Convert.ToDouble(data));
+                        break;
+                }
+            }
+            return operands.Pop();
+        }
+    }
+}
---
+++ DailyCodingProblemTests/Problem163Test.cs
@@ -0,0 +1,23 @@
+﻿using DailyCodingProblem;
+using NUnit.Framework;
+
+namespace DailyCodingProblemTests
+{
+    public class Problem163Test
+    {
+        [Test]
+        public void TestEvaluateReversePolishNotationShort()
+        {
+            double result = Problem163.EvaluateReversePolishNotation(new string[] { "5", "3", "+" });
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void TestEvaluateReversePolishNotationLarge()
+        {
+            double result = Problem163.EvaluateReversePolishNotation(new string[] 
+            { "15", "7", "1","1","+","-","/","3","*","2","1","1","+","+","-" });
+            Assert.AreEqual(5, result);
+        }
+    }
+}
