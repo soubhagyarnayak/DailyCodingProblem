7dae701c05756134cce9a41593f0ac7b9cb7019a
soubhagya <soubhagya.r.nayak@gmail.com>
2019-10-28 19:46:39 +0530

    Added solution to Problem 168: Rotate matrix
    Added solution to Problem 168: Rotate matrix

---
+++ DailyCodingProblem/Problem168.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/***
+ * This problem was asked by Facebook.
+ * Given an N by N matrix, rotate it by 90 degrees clockwise.
+ * 
+ * For example, given the following matrix:
+ [[1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9]]
+ you should return:
+ [[7, 4, 1],
+ [8, 5, 2],
+ [9, 6, 3]]
+ * Follow-up: What if you couldn't use any extra space?
+ */
+namespace DailyCodingProblem
+{
+    public static class Problem168
+    {
+        public static void RotateBy90Degrees(int[][] matrix)
+        {
+            if(matrix == null || matrix.Length < 2)
+            {
+                throw new ArgumentException("The specified matrix is not valid.");
+            }
+            if(matrix.Length != matrix[0].Length)
+            {
+                throw new ArgumentException("The specified input is not a NxN matrix");
+            }
+            int n = matrix.Length;
+            for(int i = 0; i < n / 2; i++)
+            {
+                for(int j = i; j < n-i-1; j++)
+                {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[n -j -1][i];
+                    matrix[n -j -1][i] = matrix[n -i -1][n -j -1];
+                    matrix[n -i -1][n -j -1] = matrix[j][n -i -1];
+                    matrix[j][n -i -1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ DailyCodingProblemTests/Problem168Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem
+{
+    public class Problem168Test
+    {
+        [Test]
+        public void TestMatrixRotation()
+        {
+            int[][] data = new int[][]
+            {
+                new int[]{1,2,3 },
+                new int[]{4,5,6 },
+                new int[]{7,8,9 }
+            };
+            Problem168.RotateBy90Degrees(data);
+            int[][] expected = new int[][]
+            {
+                new int[]{7,4,1},
+                new int[]{8,5,2},
+                new int[]{9,6,3}
+            };
+            Assert.AreEqual(expected, data);
+        }
+    }
+}
